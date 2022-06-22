45956ecf7a9b2e69cf9a01b8ecd75295c7fe1a5e
Anil Pujari <pujarianil4@gmail.com>
2022-06-22 15:34:30 +0530

    react app init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css'
+function App() {
+  return (
+    <div className="App">
+      Hello Ticket Lab
+    </div>
+  );
+}
+
+export default App;
